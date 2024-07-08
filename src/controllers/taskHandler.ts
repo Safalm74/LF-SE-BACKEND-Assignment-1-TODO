05c54f5c4736b4e0fb4c0bb0bb699a61975015d0
Safal raj Manandhar <safalm74@gmail.com>
2024-07-08 17:19:21 +0545

    simple execption handling

--- src/controllers/taskHandler.ts
+++ src/controllers/taskHandler.ts
@@ -1,36 +1,50 @@
-import {Request, Response} from 'express';
-import * as TaskhandlerService from '../services/taskHandlers';
-import { ITask } from '../interface/task';
+import { Request, Response } from "express";
+import * as TaskhandlerService from "../services/taskHandlers";
+import { ITask } from "../interface/task";
 
-export function readTasks(req:Request,res:Response){
-    const data=TaskhandlerService.readTasks();
-    res.json(data);
+export function readTasks(req: Request, res: Response) {
+  const data = TaskhandlerService.readTasks();
+  res.json(data);
 }
 
-export function createTask(req:Request,res:Response){
-    const {body}=req;
-    const created=TaskhandlerService.createTask(body);
-    res.json(body);
+export function createTask(req: Request, res: Response) {
+  const { body } = req;
+  try {
+    res.json({
+      Message: TaskhandlerService.createTask(body),
+    });
+  } catch (e) {
+    console.log("Error Occured", e);
+    res.json({
+      Error: e,
+    });
+  }
 }
 
-export function deleteTask(req:Request,res:Response){
-    const id=req.params.id;
-    TaskhandlerService.deleteTask(id);
-    res.json(
-        {
-            msg:"task removes sucessfully",
-            id:id
-        }
-    );
+export function deleteTask(req: Request, res: Response) {
+  const id = req.params.id;
+  try {
+    res.json({
+      msg: TaskhandlerService.deleteTask(id),
+    });
+  } catch (e) {
+    console.log("Error Occured", e);
+    res.json({
+      Error: e,
+    });
+  }
 }
-export function updatedTask(req:Request,res:Response){
-    const id=req.params.id;
-    const {body}=req;
-    TaskhandlerService.updatedTask(id,body)
-    res.json(
-        {
-            msg:"task updated sucessfully",
-            id:id
-        }
-    );
+export function updatedTask(req: Request, res: Response) {
+  const id = req.params.id;
+  const { body } = req;
+  try {
+    res.json({
+      msg: TaskhandlerService.updatedTask(id, body),
+    });
+  } catch (e) {
+    console.log("Error Occured", e);
+    res.json({
+      Error: e,
+    });
+  }
 }
--- src/models/taskHandler.ts
+++ src/models/taskHandler.ts
@@ -1,61 +1,69 @@
 import { ITask } from "../interface/task";
 
-type task={
-        id: string,
-        name: string,
-        is_finished:boolean,
+type task = {
+  id: string;
+  name: string;
+  is_finished: boolean;
+};
+let tasks: task[] = [
+  {
+    id: "1",
+    name: "wash dress",
+    is_finished: false,
+  },
+];
+
+export function readTasks() {
+  return tasks;
 }
-let tasks :task[]=[
-    {
-        id:'1',
-        name:'wash dress',
-        is_finished:false
+
+export function readReaminingTasks() {
+  const taskRemaining = tasks.filter((task) => {
+    return !task.is_finished;
+  });
+  return taskRemaining;
+}
+
+export function createTask(task: ITask) {
+  for (let taskobj of tasks) {
+    if (taskobj.name == task.name) {
+      return "Already Exists";
     }
-]
-
-
-export function readTasks(){
-    return tasks;
+  }
+  const newTaskId = `${tasks.length + 1}`;
+  const initialFinishFlag = false;
+  const newTask = {
+    id: newTaskId,
+    is_finished: initialFinishFlag,
+    ...task,
+  };
+  tasks.push(newTask);
+  return `Task Created: ${task.name}`;
 }
 
-export function readReaminingTasks(){
-    const taskRemaining=tasks.filter(
-        (task)=>{
-            return !task.is_finished
-        }
-    );
-    return taskRemaining;
+export function deleteTask(id: string) {
+  const delete_obj = tasks.find(({ id: taskId }) => {
+    return taskId === id;
+  });
+  if (delete_obj) {
+    tasks = tasks.filter(({ id: taskId }) => {
+      return !(taskId === id);
+    });
+    return ` task deleted: ${delete_obj.name}`;
+  } else {
+    return `no task with given id:${id}`;
+  }
 }
 
-export function createTask(task:ITask){
-    const newTaskId=`${tasks.length+1}`;
-    const initialFinishFlag=false;
-    const newTask={
-        id:newTaskId,
-        is_finished:initialFinishFlag,
-        ...task
-    }
-    tasks.push(newTask);
-    console.log(tasks);
-    return true;
+export function updateTask(id: string, updatedTask: ITask) {
+  const update_obj = tasks.find(({ id: taskId }) => {
+    return taskId === id;
+  });
+  if (update_obj) {
+    const temp = update_obj.name;
+    update_obj.name = updatedTask.name;
+    return ` task updated: from ${temp} to ${update_obj.name}`;
+  } else {
+    return `no task with given id:${id}`;
+  }
 }
-
-export function deleteTask(id:string){
-    tasks=tasks.filter(
-        ({id:taskId})=>{
-            return !(taskId===id);
-        }
-    )
-}
-
-export function updateTask(id:string,updatedTask:ITask){
-    for (let i=0;i<tasks.length;i++){
-        if(tasks[i].id===id){
-            console.log('updating ',id);
-            tasks[i].name=updatedTask.name;
-            return true
-        }
-    }
-    return false;
-}
-
