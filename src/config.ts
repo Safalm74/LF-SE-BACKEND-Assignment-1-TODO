72aed17d8b728188ac85c129a25d1712eeb571c3
Safal raj Manandhar <safalm74@gmail.com>
2024-07-09 17:29:08 +0545

    Correction

--- src/config.ts
+++ src/config.ts
@@ -4,10 +4,6 @@ dotenv.config();
 
 const config = {
   port: process.env.PORT,
-  POSTGRES_USER: process.env.POSTGRES_USER,
-  POSTGRES_PW: process.env.POSTGRES_PW,
-  POSTGRES_DB: process.env.POSTGRES_DB,
-  POSTGRES_PORT:process.env.POSTGRES_PORT
 };
 
 export default config;
